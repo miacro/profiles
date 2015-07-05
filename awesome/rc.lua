5f4deae7336530f0420188dd8d3f3ac2324dba46
miacro <fqguozhou@gmail.com>
2015-07-05 14:41:12 +0800

    haha

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -77,7 +77,7 @@ require("fq_wallpaper")
 tags = {}
 for s = 1, screen.count() do
   -- Each screen has its own tag table.
-  tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+  tags[s] = awful.tag({ "  1  ", "  2  ", "  3  ", "  4  ", "  5  ", "  6  ", "  7  ", "  8  ", "  9  " }, s, layouts[1])
 end
 -- }}}
 
