17454119dd1a745af1fbe383fb4eedbe7e06d6a2
miacro <fqguozhou@gmail.com>
2020-06-25 22:30:13 +0800

    ...

--- docs/vps/install-v2ray-plugin.sh
+++ docs/vps/install-v2ray-plugin.sh
@@ -5,4 +5,4 @@ FILENAME=v2ray-plugin-linux-amd64
 FETCH_URL=https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.1/${FILENAME}-v${VERSION}.tar.gz
 curl -fSL ${FETCH_URL} -o /tmp/${FILENAME}-v${VERSION}.tar.gz
 tar -xvzf /tmp/${FILENAME}-v${VERSION}.tar.gz -C /tmp
-[[ -f /tmp/v2ray-plugin_linux_amd64  ]] && mv /tmp/v2ray-plugin_linux_amd64 /usr/local/bin/
+[[ -f /tmp/v2ray-plugin_linux_amd64  ]] && mv /tmp/v2ray-plugin_linux_amd64 /usr/local/bin/v2ray-plugin
