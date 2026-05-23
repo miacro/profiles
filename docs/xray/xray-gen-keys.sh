392e24dee7ed8d8f4fa12d6cf095e6a51208c7fb
miacro <fqguozhou@gmail.com>
2026-05-23 21:11:42 +0800

    update xray

--- docs/xray/xray-gen-keys.sh
+++ docs/xray/xray-gen-keys.sh
@@ -2,14 +2,10 @@
 
 
 xray_keys_file=./xray-keys
-echo "xray uuid:" > ${xray_keys_file}
-echo -e $(xray uuid) >> ${xray_keys_file}
+echo -e "xray uuid:\n$(xray uuid)" > ${xray_keys_file}
 
-echo -e "\n\nxray x25519:" >> ${xray_keys_file}
-echo -e $(xray x25519) >> ${xray_keys_file}
+echo -e "\n\nxray x25519:\n$(xray x25519)" >> ${xray_keys_file}
 
-echo -e "\n\nshortId:" >> ${xray_keys_file}
-echo -e $(openssl rand -hex 8) >> ${xray_keys_file}
+echo -e "\n\nshortId:\n$(openssl rand -hex 8)" >> ${xray_keys_file}
 
-echo -e "\n\nhysteria auth" >> ${xray_keys_file}
-echo -e $(openssl rand -hex 12) >> ${xray_keys_file}
+echo -e "\n\nhysteria auth:\n$(openssl rand -hex 12)" >> ${xray_keys_file}
