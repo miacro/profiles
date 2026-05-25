5f2aaed9e7dc8e98caa3566a62d0d3722b6d4f64
miacro <fqguozhou@gmail.com>
2026-05-25 11:24:00 +0800

    add tuic for xray

--- docs/xray/xray-gen-keys.sh
+++ docs/xray/xray-gen-keys.sh
@@ -9,3 +9,6 @@ echo -e "\n\nxray x25519:\n$(xray x25519)" >> ${xray_keys_file}
 echo -e "\n\nshortId:\n$(openssl rand -hex 8)" >> ${xray_keys_file}
 
 echo -e "\n\nhysteria auth:\n$(openssl rand -hex 12)" >> ${xray_keys_file}
+
+echo -e "\n\ntuic uuid:\n$(xray uuid)" >> ${xray_keys_file}
+echo -e "\n\ntuic password:\n$(openssl rand -hex 16)" >> ${xray_keys_file}
